80a0a4f9d9684e1b09987f26c7561b6bbceaf825
Replit user <>
2023-10-05 18:54:32 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,15 +43,43 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int is_valid(Node* n){
+/*
+1.Cree una función que a partir de un nodo genere una **lista con los nodos adyacentes**:
 
-    return 1;
-}
+Recuerde que los nodos adyacentes son generados aplicando las acciones al estado actual.
 
+> - Para el caso del ejemplo, la función debería retornar una lista con **9 nodos**. Cada uno de ellos idéntico al nodo original pero cambiando el valor de la primera casilla vacía, es decir: sudo[0][2], por 1, 2, ..., 9.
+> - Utilice la función Node* copy(Node* n) para copiar nodos.
+
+*/
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
+  /*
+    obtenga los nodos adyacentes a n
+    y agréguelos a la lista
+  */
+    printf("Estoy listo!");
+  
     return list;
+}
+
+/*
+2.Cree la función **int is_valid(Node * n)**, para validar si un estado/nodo es válido (cumple con las restricciones del problema). Debe validar que:
+
+- No se repitan números en las filas
+- No se repitan números en las columnas
+- No se repitan números en las submatrices de 3x3
+
+Si el estado es válido la función retorna 1, si no lo es retorna 0.
+
+> Para marcar los números que vayan apareciendo en una fila/columna/submatriz puede usar un arreglo de enteros de largo 10 inicializado con 0s. Cada vez que aparezca un número i, verifique que la casilla i del arreglo sea igual a 0, luego márquela con un '1'. Si la casilla es '1' quiere decir que el número ya estaba marcado por lo que la fla/columna/submatriz no es válida.
+
+*/
+
+int is_valid(Node* n){
+
+    return 1;
 }
 
 
@@ -65,7 +93,6 @@ Node* DFS(Node* initial, int* cont){
 
 
 
-/*
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -76,4 +103,4 @@ int main( int argc, char *argv[] ){
   print_node(final);
 
   return 0;
-}*/
+}
