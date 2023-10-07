1952af6fe7bbbcc278588e5d217ce5c6c7207f58
Replit user <>
2023-10-07 01:43:59 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -56,9 +56,9 @@ Si el estado es válido la función retorna 1, si no lo es retorna 0.
 */
 
 int is_valid(Node* n){
-  int fila[9][10] = {0}; 
-  int columna[9][10] = {0}; 
-  int submatriz[9][10] = {0}; 
+  int fila[9][9] = {0}; 
+  int columna[9][9] = {0}; 
+  int submatriz[9][9] = {0}; 
 
   for (int i = 0; i < 9; i++) 
   {
